57fa7761bd3e79f7e55513acce813cf85c7f739a
tmarmey <thibaut.marmey@gmail.com>
2018-10-18 12:01:58 +0200

    update cours R, gestin de données avec les facteurs

---
+++ Cours R/Test/Chapitre7 - OCR/Chapitre7.R
@@ -0,0 +1,7 @@
+#communG = intersect(names(performanceG), names(tailleG))
+#performanceG2 = performanceG[communG]
+#tailleG2 = tailleG[communG]
+
+#performanceG_tri = performanceG2[order(performanceG2)]
+#tailleG_tri = tailleG2[order(performanceG2)]
+
